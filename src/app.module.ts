c1a9b5bb8d3af26f7c8fd3ccd125ab69fb8e351f
Marcos Barbosa Evangelista <marcos.evangelista@compasso.com.br>
2023-10-26 17:29:55 -0300

    Primeiro commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/meubanco'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+    }),
+    UsersModule,
+  ]
+})
+export class AppModule {}
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,13 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => String)
+  teste() {
+    return "TESTE GRAPHQL";
+  }
+}
